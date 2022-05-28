26bf8fab832b6ae8314d86a9c2538eedda920f8b
marcellmittnacht <marcellmittnacht@gmail.com>
2022-05-28 10:43:38 +0200

    revert

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cp -r ~/.config/nvim .
+cp -r ~/.config/alacritty .
+cp -r ~/.config/i3 .
+cp -r ~/.config/i3status .
+cp -r ~/.config/picom .
+cp ~/.tmux.conf .
 
 git add .
 git commit -m "dotfiles update"
