d487b584aa32ea4ab21316df9d98df23b1fe2896
marcellmittnacht <marcellmittnacht@gmail.com>
2022-06-02 21:28:23 +0200

    dotfiles update

--- nvim/lua/lsp_conf.lua
+++ nvim/lua/lsp_conf.lua
@@ -5,6 +5,9 @@ local on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true }
 
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
